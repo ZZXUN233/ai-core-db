9e7cf2c591846a276aa00af16b3d6103075a76bc
zzxun <zzxun233@outlook.com>
2025-07-21 22:20:06 +0800

    init: 初次提交

---
+++ src/main/java/com/zzxun/ai_core_bd/AiCoreBdApplication.java
@@ -0,0 +1,13 @@
+package com.zzxun.ai_core_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoreBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCoreBdApplication.class, args);
+	}
+
+}
