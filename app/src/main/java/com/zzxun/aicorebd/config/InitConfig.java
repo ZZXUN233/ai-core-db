d1893bda66776daaa4190a804e21399cefc9c2ad
zzxun <zzxun233@outlook.com>
2025-07-27 14:56:50 +0800

    feat: ollama本地联调支持

---
+++ app/src/main/java/com/zzxun/aicorebd/config/InitConfig.java
@@ -0,0 +1,25 @@
+package com.zzxun.aicorebd.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
+
+public class InitConfig {
+
+//
+//    @Bean
+//    CommandLineRunner cli(ChatClient.Builder builder) {
+//        return args -> {
+//            var chat = builder.build();
+//            var scanner = new Scanner(System.in);
+//            System.out.println("\nLet's chat!");
+//            while (true) {
+//                System.out.print("\nUSER: ");
+//                System.out.println("ASSISTANT: " +
+//                        chat.prompt(scanner.nextLine()).call().content());
+//            }
+//        };
+//    }
+}
--- app/src/main/java/com/zzxun/aicorebd/controller/ChatController.java
+++ app/src/main/java/com/zzxun/aicorebd/controller/ChatController.java
@@ -1,24 +1,36 @@
 package com.zzxun.aicorebd.controller;
 
-import org.apache.logging.log4j.message.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/chat")
 public class ChatController {
 
-    @Autowired
-    private ChatClient chatClient;
+    private final OllamaChatModel chatModel;
 
-    @GetMapping
-    public String chat(@RequestParam String message) {
-        var response = chatClient.call(List.of(new Message(Role.USER, message)));
-        return response.getResult().getOutput().getContent();
+    @Autowired
+    public ChatController(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
     }
+
+    @GetMapping("/ai/generate")
+    public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+
 }
